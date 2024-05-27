7bac34b728a9d1057c69825cdaa043aed34cfd5e
Leonie von Mann <s8levonm@stud.uni-saarland.de>
2024-05-27 19:57:06 +0200

    Add feature extraction for NOK measurements

--- feature_extraction.py
+++ feature_extraction.py
@@ -68,48 +68,61 @@ def extract_all_features(data, sampling_rate):
     all_features = {**time_features, **frequency_features, **wavelet_features}
     return all_features
 
+def extract_all_combined_features(ae_data, current_data, anomaly):
+    features_ae = extract_all_features(ae_data.values.flatten(), sampling_rate=2000)
+    features_rms = extract_all_features(
+        current_data["Irms_Grinding_rate100000_clipping0_batch0"], sampling_rate=1000)
+    features_li1 = extract_all_features(
+        current_data["Grinding spindle current L1_rate100000_clipping0_batch0"],
+        sampling_rate=1000)
+    features_li2 = extract_all_features(
+        current_data["Grinding spindle current L2_rate100000_clipping0_batch0"],
+        sampling_rate=1000)
+    features_li3 = extract_all_features(
+        current_data["Grinding spindle current L3_rate100000_clipping0_batch0"],
+        sampling_rate=1000)
+
+    features_ae = {f"ae_{k}": v for k, v in features_ae.items()}
+    features_rms = {f"rms_{k}": v for k, v in features_ae.items()}
+    features_li1 = {f"li1_{k}": v for k, v in features_ae.items()}
+    features_li2 = {f"li2_{k}": v for k, v in features_ae.items()}
+    features_li3 = {f"li3_{k}": v for k, v in features_ae.items()}
+
+    combined_features = {**features_ae, **features_rms, **features_li1, **features_li2,
+                            **features_li3}
+    combined_features["anomaly"] = anomaly
+
+    return combined_features
+
+def extract_features_from_path(path, anomaly):
+    all_features_list = []
+
+    # Iterate through each folder in the base directory
+    for folder_name in os.listdir(path):
+        folder_path = os.path.join(path, folder_name)
+        raw_path = os.path.join(folder_path, "raw")
+
+        if os.path.isdir(raw_path):
+            for file_name in os.listdir(raw_path):
+                file_path = os.path.join(raw_path, file_name)
+                if "2000KHz" in file_name:
+                    ae_data = pd.read_parquet(file_path)
+                else:
+                    current_data = pd.read_parquet(file_path)
+
+            combined_features = extract_all_combined_features(ae_data, current_data, anomaly)
+
+            all_features_list.append(combined_features)
+
+    return pd.DataFrame(all_features_list)
+
 #--------------------------------------------------------------
 
 OK_DIRECTORY = '../Data/OK_Measurements'
-all_features_list = []
+NOK_DIRECTORY = '../Data/NOK_Measurements'
 
-# Iterate through each folder in the base directory
-for folder_name in os.listdir(OK_DIRECTORY):
-    folder_path = os.path.join(OK_DIRECTORY, folder_name)
-    raw_path = os.path.join(folder_path, "raw")
+all_features_df_ok = extract_features_from_path(OK_DIRECTORY, False)
+all_features_df_nok = extract_features_from_path(NOK_DIRECTORY, True)
 
-    if os.path.isdir(raw_path):
-        for file_name in os.listdir(raw_path):
-            file_path = os.path.join(raw_path, file_name)
-            if "2000KHz" in file_name:
-                ae_data = pd.read_parquet(file_path)
-            else:
-                current_data = pd.read_parquet(file_path)
-
-        features_ae = extract_all_features(ae_data.values.flatten(), sampling_rate=2000)
-        features_rms = extract_all_features(
-            current_data["Irms_Grinding_rate100000_clipping0_batch0"], sampling_rate=1000)
-        features_li1 = extract_all_features(
-            current_data["Grinding spindle current L1_rate100000_clipping0_batch0"],
-            sampling_rate=1000)
-        features_li2 = extract_all_features(
-            current_data["Grinding spindle current L2_rate100000_clipping0_batch0"],
-            sampling_rate=1000)
-        features_li3 = extract_all_features(
-            current_data["Grinding spindle current L3_rate100000_clipping0_batch0"],
-            sampling_rate=1000)
-
-        features_ae = {f"ae_{k}": v for k, v in features_ae.items()}
-        features_rms = {f"rms_{k}": v for k, v in features_ae.items()}
-        features_li1 = {f"li1_{k}": v for k, v in features_ae.items()}
-        features_li2 = {f"li2_{k}": v for k, v in features_ae.items()}
-        features_li3 = {f"li3_{k}": v for k, v in features_ae.items()}
-
-        combined_features = {**features_ae, **features_rms, **features_li1, **features_li2,
-                             **features_li3}
-        combined_features["anomaly"] = False
-
-        all_features_list.append(combined_features)
-
-all_features_df = pd.DataFrame(all_features_list)
+all_features_df = pd.concat([all_features_df_ok, all_features_df_nok], ignore_index=True)
 all_features_df.to_parquet('features.parquet', engine='pyarrow')
