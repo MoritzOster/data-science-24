47776116a4bd07cce427a5972040221da827e1e4
Moritz Oster <Moritz Oster>
2024-06-04 16:29:59 +0100

    ae visualization

--- dashboard/data_provider.py
+++ dashboard/data_provider.py
@@ -14,19 +14,26 @@ class DataProvider:
         # e.g. 2 MHz = 2e6 samples per second -> update every 1 / 2e6 seconds
         self.sleep_time = downsample_factor / sampling_rate
 
-    def start(self):
-        while self.current_index < self.downsampled_total_points:
-            current_measurement = self.downsampled_data.iloc[self.current_index]
-            self.current_index += 1
-            # Print how many percent of the data has been processed
-            print(f"{self.current_index / self.downsampled_total_points * 100:.2f}% processed")
-            time.sleep(self.sleep_time)
-            yield current_measurement
+    def next(self):
+        if (self.current_index == self.downsampled_total_points):
+            return
+        current_measurement = self.downsampled_data.iloc[self.current_index]
+        self.current_index += 1
+        # Print how many percent of the data has been processed
+        # print(f"{self.current_index / self.downsampled_total_points * 100:.2f}% processed")
+        # Sleep to simulate real-time data
+        time.sleep(self.sleep_time)
+        # print (current_measurement)
+        return current_measurement
 
-# Usage example:
+# Usage example
 # file_path = './2024.02.14_22.00.40_Grinding/raw/Sampling2000KHz_AEKi-0.parquet'
 # sampling_rate = 2e6  # 2 MHz
 # downsample_factor = 1000
 # data_provider = DataProvider(file_path, sampling_rate, downsample_factor)
-# for measurement in data_provider.start():
-#     print(measurement)
+
+# while (1):
+#     next_measurement = data_provider.next()
+#     if next_measurement is None:
+#         break
+#     print(next_measurement)
