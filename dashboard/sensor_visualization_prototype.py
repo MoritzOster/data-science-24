a8448ba34a1486c925f8d4b56779d21054aa8648
Moritz Oster <Moritz Oster>
2024-06-04 11:46:49 +0100

    first working version

--- dashboard/sensor_visualization_prototype.py
+++ dashboard/sensor_visualization_prototype.py
@@ -3,17 +3,13 @@ import pandas as pd
 import numpy as np
 import time
 
+
 @st.cache_data
 def load_data(file_path):
     return pd.read_parquet(file_path)
 
-def simulate_real_time_data(data, batch_size):
-    for start in range(0, len(data), batch_size):
-        end = min(start + batch_size, len(data))
-        yield data.iloc[start:end]
 
 file_path = './2024.02.14_22.00.40_Grinding/raw/Sampling2000KHz_AEKi-0.parquet'
-
 data = load_data(file_path)
 
 st.title('Real-Time AE Signal Visualization')
@@ -21,12 +17,34 @@ st.write("Simulating real-time data from AE measurements.")
 
 chart = st.line_chart()
 
-batch_size = 2000 * 1  # 1 second worth of data
-total_points = len(data)
-current_time = 0
 
-for batch in simulate_real_time_data(data, batch_size=batch_size):
-    chart.add_rows(batch)
-    current_time += len(batch) / 2000  
-    time.sleep(0.001)  
+downsample_factor = 1000
+sampling_rate = 2e6
+# Calculate sleep time based on desired animation speed
+sleep_time = downsample_factor / sampling_rate  
+
+downsampled_data = data[::downsample_factor]
+
+total_points_after_downsampling = len(downsampled_data)
+current_index = 0
+
+update_batch_size = 100
+update_batch = []
+# progress = st.progress(0)  # Initialize progress bar
+
+while current_index < total_points_after_downsampling:
+    current_measurement = downsampled_data.iloc[current_index]
+    update_batch.append(current_measurement)
+    current_index += 1
+    # Sleep to simulate real-time data
+    time.sleep(sleep_time)
+
+    if len(update_batch) < update_batch_size:
+            continue
+
+    chart.add_rows(update_batch)  
+    update_batch = []  
+    
+
+
 st.write("Simulation complete.")
