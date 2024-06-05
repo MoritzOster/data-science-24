c49c8d78cf1509b6b77c619b82deb4c393c287ef
Moritz Oster <Moritz Oster>
2024-06-05 16:38:39 +0100

    added visual for lrms

--- dashboard/amplitude_graph.py
+++ dashboard/amplitude_graph.py
@@ -17,18 +17,21 @@ class AmplitudeGraph:
         self.update_batch = []
         return True
 
-left_column, right_column = st.columns(2)
+# left_column, right_column = st.columns(2)
+left_column, right_column = st, st
 
-ae_file_path = './2024.02.14_22.00.40_Grinding/raw/Sampling2000KHz_AEKi-0.parquet'
+# ae_file_path = './2024.02.14_22.00.40_Grinding'
 ae_graph = AmplitudeGraph(left_column, update_batch_size= 100)
 
-lrms_grinding_path = './2024.02.14_22.00.40_Grinding/raw/Sampling2000KHz_AEKi-0.parquet'
+# lrms_grinding_path = './2024.02.14_22.00.40_Grinding/raw/Sampling2000KHz_AEKi-0.parquet'
 lrms_grinding_graph = AmplitudeGraph(right_column, update_batch_size= 100)
-data_provider = DataProvider('./2024.02.14_22.00.40_Grinding/raw/Sampling2000KHz_AEKi-0.parquet', sampling_rate = 2e6, downsample_factor = 1000)
+# data_provider = DataProvider('./raw/Sampling2000KHz_AEKi-0.parquet', sampling_rate = 2e6, downsample_factor = 1000)
+
+data_provider = DataProvider('./2024.02.14_22.00.40_Grinding', downsample_factor = 1000)
 while (1):
-    next_measurement = data_provider.next()
-    if next_measurement is None:
+    ae_sample, current_sample = data_provider.next()
+    if (ae_sample is None) | (current_sample is None):
         break
-    ae_graph.update_graph(next_measurement) 
-    lrms_grinding_graph.update_graph(next_measurement)
+    ae_graph.update_graph(ae_sample) 
+    lrms_grinding_graph.update_graph(current_sample)
         
--- dashboard/data_provider.py
+++ dashboard/data_provider.py
@@ -1,30 +1,44 @@
 import pandas as pd
 import time
 
+# DEST: /raw/Sampling2000KHz_AEKi-0.parquet
+
 class DataProvider:
-    def __init__(self, file_path, sampling_rate, downsample_factor):
-        self.data = pd.read_parquet(file_path)
-        self.total_points = len(self.data)
-        self.sampling_rate = sampling_rate
+    def __init__(self, file_path, downsample_factor):
+        ae_data = pd.read_parquet(file_path+'/raw/Sampling2000KHz_AEKi-0.parquet')
+        current_data = pd.read_parquet(file_path+'/raw/Sampling100KHz_Irms_Grinding-Grinding spindle current L1-Grinding spindle current L2-Grinding spindle current L3-0.parquet')
+        current_lrms_data = current_data['Irms_Grinding_rate100000_clipping0_batch0']
+
+        # Assuming a sampling rate of 2 MHz for ae and 0.1 MHz for lrms
+        ae_downsample_factor = downsample_factor
+        current_downsample_factor = ae_downsample_factor // 20
+        sampling_rate = 2e6
+
         # Downsample the data by only considering every downsample_factor element
-        self.downsampled_data = self.data[::downsample_factor]
-        self.downsampled_total_points = len(self.downsampled_data)
-        self.current_index = 0
+        self.downsampled_ae_data = ae_data[::ae_downsample_factor]
+        self.downsampled_current_data = current_lrms_data[::current_downsample_factor]
+
+        self.downsampled_total_points = min(len(self.downsampled_ae_data), len(self.downsampled_current_data))
+        self.index = 0
         # Downsample factor = 1 -> update for every sample taken (at the sampling rate)
         # e.g. 2 MHz = 2e6 samples per second -> update every 1 / 2e6 seconds
+        # Works for both ae and current data, since current data is downsampled by a factor of 20 less
         self.sleep_time = downsample_factor / sampling_rate
 
+    def meta_data(self):
+        pass
+
     def next(self):
-        if (self.current_index == self.downsampled_total_points):
+        if (self.index == self.downsampled_total_points):
             return
-        current_measurement = self.downsampled_data.iloc[self.current_index]
-        self.current_index += 1
+        ae_sample = self.downsampled_ae_data.iloc[self.index]
+        current_sample = self.downsampled_current_data.iloc[self.index]
+        self.index += 1
         # Print how many percent of the data has been processed
-        # print(f"{self.current_index / self.downsampled_total_points * 100:.2f}% processed")
+        # print(f"{self.index / self.downsampled_total_points * 100:.2f}% processed")
         # Sleep to simulate real-time data
         time.sleep(self.sleep_time)
-        # print (current_measurement)
-        return current_measurement
+        return ae_sample, current_sample
 
 # Usage example
 # file_path = './2024.02.14_22.00.40_Grinding/raw/Sampling2000KHz_AEKi-0.parquet'
