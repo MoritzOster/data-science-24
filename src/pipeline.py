8ee8a7a01fc54610f1b20814bf36fde581832dea
Moritz Oster <s8mooste@stud.uni-saarland.de>
2024-06-16 14:05:26 +0200

    Added pyarrow to Dockerfile

---
+++ src/pipeline.py
@@ -0,0 +1,14 @@
+
+#Extract Features from the data
+
+#Split the data / define the split ratio
+
+#Upsample the NOK Data to balance the dataset
+
+#Use TPOT - genetic Programming to find the best model#
+
+#Train the model
+
+#Export the Best model
+
+#Evaluate the model
