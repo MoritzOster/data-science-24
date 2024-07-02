d8595e1e05f2995a6dc96beb5b1cb218dfb0aacf
Moritz Oster <s8mooste@stud.uni-saarland.de>
2024-07-02 16:11:36 +0200

    added csv save of predictions

--- src/app.py
+++ src/app.py
@@ -1,39 +1,32 @@
 import sys
 import os
+import csv
 from data_provider import DataProvider
 import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 import time
 from prodetect import prodetect_predict
+from datetime import datetime
 
 # Calculate the path to the src directory
 base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 src_path = os.path.join(base_dir, 'src')
 sys.path.insert(0, src_path)
 
-
-
-
 data_path_NOK = '../data/example_recordings/NOK_Measurements'
 data_path_OK = '../data/example_recordings/OK_Measurements'
-
 
 # example_ae_path = data_path_OK + '/2024.02.14_22.00.40_Grinding'
 example_ae_path = data_path_NOK + '/2024.02.15_02.27.22_Grinding'
 # data/example_recordings/NOK_Measurements/2024.02.15_02.27.22_Grinding
 data_provider = DataProvider(example_ae_path, downsample_factor=100000)
 
-
-
-
 # Assuming data_provider is already imported and configured
 def load_data():
     while True:
         data = data_provider.next()
         yield data
-
-
 
 def plot_data():
     fig, ax = plt.subplots()
@@ -48,21 +41,31 @@ def plot_data():
         if data is None:
             break
         elapsed_time = time.time() - start_time
-        if elapsed_time > 60:  # Stop after 20 seconds
+        if elapsed_time > 60:  # Stop after 60 seconds
             break
         line.set_xdata(np.append(line.get_xdata(), elapsed_time))
         line.set_ydata(np.append(line.get_ydata(), data[0]))
         ax.figure.canvas.draw_idle()
         placeholder.pyplot(fig)
-        # time.sleep(0.02)  # Adjust based on desired streaming rate
 
 st.title('Real-time Data Streaming')
 status_placeholder = st.empty()
 plot_data()
 
-anomaly_prediction = prodetect_predict(example_ae_path+ '/raw/Sampling2000KHz_AEKi-0.parquet')
-print (anomaly_prediction)
+anomaly_prediction = prodetect_predict(example_ae_path + '/raw/Sampling2000KHz_AEKi-0.parquet')
+print(anomaly_prediction)
 result = anomaly_prediction
+timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+csv_filename = 'predictions.csv'
+
+# Write prediction results to CSV
+with open(csv_filename, mode='a', newline='') as file:
+    writer = csv.writer(file)
+    # Write header if file is empty
+    if file.tell() == 0:
+        writer.writerow(['Timestamp', 'AnomalyPrediction', 'Path'])
+    writer.writerow([timestamp, result[0], example_ae_path])
+
 if result[0]:
     status_placeholder.error('**Failure!**', icon="🚨")
 else:
